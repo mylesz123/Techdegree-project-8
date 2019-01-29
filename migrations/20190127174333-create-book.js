5ce756d1bf9350d1fb486b3d5ed356a9e17068be
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-29 16:22:38 -0500

    title, author, genre and year properties in db

---
+++ migrations/20190127174333-create-book.js
@@ -0,0 +1,39 @@
+/*'up', change db from old to new state
+queryInterface - responsible for changing the db 
+*/
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      genre: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => { //undo
+    return queryInterface.dropTable('Books');
+  }
+};
