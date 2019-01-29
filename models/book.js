a53c3f6e7d7751205dc23408b9b7f0901cb7ac86
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-29 16:23:16 -0500

    models folder

---
+++ models/book.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+
+    title: {
+      type: DataTypes.STRING,
+      validate: {// validate checks upon submit
+        notEmpty: { // that text field better not be blank
+          msg: 'Enter Book Title'
+        }
+      }
+    },
+
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Enter Author'
+        }
+      }
+    },
+
+    genre: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Enter Genre'
+        }
+      }
+    },
+
+    year: {
+      type: DataTypes.STRING,
+      validate: {
+        isNumeric: {
+          msg: 'Year must be numeric'
+        },
+        notEmpty: {//must be a number
+          msg: "Must Enter Year"
+        }
+      }
+    },
+
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  });
+
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+
+  return Book;
+};
